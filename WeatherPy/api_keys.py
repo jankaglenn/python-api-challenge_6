565e23df43d6edd0db61e690b32f01c508d3c0e5
FIRST_NAME LAST_NAME <jankaglenn@gmail.com>
2023-09-30 22:16:05 -0400

    complete

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "fc5496a0f2efedd8ac4e8653bcab933f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0875b86e99584b45b3dc16cf4b9b32ca"
